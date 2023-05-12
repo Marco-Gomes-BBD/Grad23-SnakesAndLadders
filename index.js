44752d4fb7e440150440c7cdb6cf3143ef8f4767
Hewitt Nyambalo <hewitt@bbd.co.za>
2023-05-12 17:11:10 +0200

    checkpoint

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-let template_doc = document.getElementById("player-card-template")
 
-class PlayerCard extends HTMLElement{
-    constructor(){
-        super()
-        let shadowRoot = this.attachShadow({mode: 'open'});
-        shadowRoot.appendChild(tmpl.content.cloneNode(true));
-    }
-}
+const express = require('express');
+const path = require('path');
 
-window.customElements.define('player-card', PlayerCard)
+const app = express();
+const port = process.env.PORT || 8080;
 
-console.log("Olo")
+// sendFile will go here
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'html/pages/home_page.html'));
+});
+
+app.get('/game', function(req, res) {
+    res.sendFile(path.join(__dirname, 'html/pages/game_page.html'));
+});
+
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
