0e1bd564b28a411884324d06508fa325710ce3b1
Hewitt Nyambalo <hewitt@bbd.co.za>
2023-05-13 14:04:21 +0200

    reconfigure to serve through node

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'html/pages/home_page.html'));
+app.use(express.static("."))
+
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname, 'html/pages/home_page.html'));
 });
 
 app.get('/game', function(req, res) {
     res.sendFile(path.join(__dirname, 'html/pages/game_page.html'));
 });
 
+app.get('/player-select', function(req, res) {
+    res.sendFile(path.join(__dirname, "html/pages/player_select.html"))
+});
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
