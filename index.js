537c25f74ea39c80cc7acaa664790779b4248aa5
Hewitt Nyambalo <122514230+hewitt-BBD@users.noreply.github.com>
2023-05-16 13:02:40 +0200

    Express (#2)
    * add basic home page layout
    * add colour
    * checkpoint
    * reconfigure to serve through node
    * rename stylesheets
    * touch ups
    * Update index.js
    
    Co-authored-by: Marco-Gomes-BBD <marcog@bbd.co.za>
    * Update package.json
    
    Co-authored-by: Marco-Gomes-BBD <marcog@bbd.co.za>
    * Feedback changes

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static("."))
+
+app.get('/home', function(_req, res) {
+    res.sendFile(path.join(__dirname, 'html/pages/home_page.html'));
+});
+
+app.get('/game', function(_req, res) {
+    res.sendFile(path.join(__dirname, 'html/pages/game_page.html'));
+});
+
+app.get('/player-select', function(_req, res) {
+    res.sendFile(path.join(__dirname, "html/pages/player_select.html"))
+});
+
+app.listen(port, () => {
+    console.log('Server started at http://localhost:' + port);
+});
