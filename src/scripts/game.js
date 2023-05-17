8125087843d1012e7fecd2bd45bcc1c386e064f5
Marco Gomes <marcog@bbd.co.za>
2023-05-17 10:42:55 +0200

    Simplify board drawing

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,3 +1,5 @@
+let context = undefined;
+
 function initBoard() {
     const canvas = document.getElementById('board');
     context = canvas.getContext('2d');
@@ -10,57 +12,41 @@ function initBoard() {
 }
 
 function drawBoard() {
-    // size of each square
-    const squareSize = 60;
-    // let  colors = [ "#e91c49", "#cfcc1c", "#1281db", "#3db473"];
+    // Parameters
+    const gridWidth = 10;
+    const gridHeight = 10;
+    const gridSize = gridWidth * gridHeight;
 
-    // draw squares
-    for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
-            // randomise square colors
-            // context.fillStyle =  colors[Math.floor(Math.random() * colors.length)];
-            context.fillStyle = (i + j) % 2 == 0 ? 'white' : 'black';
-            let xOffset = j * squareSize;
-            let yOffset = i * squareSize;
+    const squareSize = 60;
+    const colors = ['white', 'black'];
+
+    // Draw squares
+    for (let y = 0; y < gridHeight; y++) {
+        for (let x = 0; x < gridWidth; x++) {
+            const index = y + x;
+            context.fillStyle = colors[index % colors.length];
+            let xOffset = x * squareSize;
+            let yOffset = y * squareSize;
             context.fillRect(xOffset, yOffset, squareSize, squareSize);
         }
     }
-    // number placement
+    // Draw numbers
+    const fontSize = 11;
     context.fillStyle = 'magenta';
-    context.font = 'bold 11px Arial';
-    // first row
-    for (let c = 1; c < 11; c++) {
-        context.fillText(c, (c - 1) * 60, 550);
-    }
-    for (let c = 21; c < 31; c++) {
-        context.fillText(41 - c, (c - 21) * 60, 490);
-    }
-    for (let d = 0; d < 4; d++) {
-        for (let c = 21 + d * 10; c < 31 + d * 10; c++) {
-            context.fillText(
-                c + d * 10,
-                (c - (21 + d * 10)) * 60,
-                430 - d * 120
-            );
-        }
-        for (let c = 31 + d * 10; c < 41 + d * 10; c++) {
-            context.fillText(
-                71 + d * 30 - c,
-                (c - (31 + d * 10)) * 60,
-                370 - d * 120
-            );
-        }
+    context.font = `bold ${fontSize}px Arial`;
+    for (let i = 0; i < gridSize; i++) {
+        const x = i % gridWidth;
+        const y = Math.floor(i / gridWidth);
+        const xOffset = (y % 2 == 0 ? x : gridWidth - x - 1) * squareSize;
+        const yOffset = (gridHeight - y - 1) * squareSize;
+        context.fillText(i + 1, xOffset, yOffset + fontSize);
     }
 
-    // start and finish squares
+    // Start and finish squares
     context.font = 'bold 15px Arial';
     context.fillStyle = 'magenta';
     context.fillText('Start', 13, 570);
     context.fillText('Finish', 13, 30);
-
-    // draw the border around the
-    // context.strokeStyle = "black";
-    // context.strokeRect(boardTopx, boardTopy, squareSize*8, squareSize*8)
 }
 
 // @params : (x1, y1) - start x and y coordinates
@@ -72,13 +58,11 @@ function drawLadder(x1, y1, x2, y2) {
     var dx = x2 - x1;
     var dy = y2 - y1;
     var radianAngle = Math.atan2(dy, dx);
-    var midX = (x2 + x1) / 2;
-    var midY = (y2 + y1) / 2;
 
     context.strokeStyle = 'green';
     context.lineWidth = 6;
     context.save();
-    // context.translate(midX,midY);
+
     radianAngle < 0 ? context.rotate(radianAngle) : null;
     context.beginPath();
     context.moveTo(x1, y1);
@@ -86,11 +70,6 @@ function drawLadder(x1, y1, x2, y2) {
     context.moveTo(x1 + width, y1);
     context.lineTo(x1 + width, y1 + height);
     context.stroke();
-    // context.moveTo(x1, y1);
-    // context.lineTo(x2, y2);
-    // context.moveTo((x1 + width), y1);
-    // context.lineTo(x2 + width, y2);
-    // context.stroke();
 
     let numSteps = height / 19;
 
@@ -102,34 +81,12 @@ function drawLadder(x1, y1, x2, y2) {
         xOffset = x1;
         yOffset = stepOffset + i * 20;
         console.log(y1 - height, numSteps, height, radianAngle);
-        // xOffset = x1 + ((i-1)*6);
-        // yOffset = (y1 *i) + 7;
 
         context.moveTo(xOffset, yOffset);
         context.lineTo(xOffset + width, yOffset);
-
-        // context.moveTo(xOffset , yOffset);
-        // context.lineTo(xOffset + width, yOffset);
         context.stroke();
     }
 
     context.closePath();
     context.restore();
-    // context.fillRect(x, y, 6, height);
-    // context.fillRect(x + width, y, 6, height);
 }
-
-// function drawLadder(x, y, width, height){
-//   // const width = 30;
-
-//   context.fillStyle = 'green';
-
-//   context.fillRect(x, y, 6, height);
-//   context.fillRect(x + width, y, 6, height);
-
-//   // draw steps
-//   for(let i = 20; i <100; i +=15){
-//         context.fillRect(x, i, 30, 6);
-//   }
-
-// }
