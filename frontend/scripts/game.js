0bf1c696113e0e10b90f190bab05be9cf1a9df28
Marco Gomes <marcog@bbd.co.za>
2023-05-28 16:33:46 +0200

    Formatting

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -315,7 +315,7 @@ function advanceGame(count) {
     let game = JSON.parse(localStorage.getItem('game_summary'));
     let details = JSON.parse(localStorage.getItem('user-details'));
     game.roll.count = count;
-    
+
     localStorage.setItem('game_summary', JSON.stringify(game));
     if (details != null) {
         fetch(
